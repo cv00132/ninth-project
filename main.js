ce41e4c4a1e6f8b2d711590d0e607e2f214417fa
Cory Vaughan <codercory13@gmail.com>
2017-02-13 18:58:10 -0500

    Populated the page with icons and labels.

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
-var information;
-
-function getInfo (callback) {
-  information = $.ajax({
-    url: `http://json-data.herokuapp.com/forms`,
-    dataType: "json",
-    success: callback
-  });
+function getInfo(callback) {
+    $.ajax({
+        url: `http://json-data.herokuapp.com/forms`,
+        dataType: "json",
+        success: callback
+    });
 }
 
-var formData;
+var labels = "";
+var icons = "";
 
-function formInfo (){
- for (var i = 0; i <information.responseJSON.length; i++){
-    formData += information.responseJSON[i].label
+function formData(data) {
+  var information = data;
+    for (var i = 0; i < information.length; i++) {
+      if ("type" === "select") {
+
+      } else if ("type") {
+
+      }
+        console.log(labels);
+        $(".form").append(`
+          <div class="inputs">
+          <i class= "small-icon fa ${information[i].icon}"</i> ${information[i].label}
+          </div>`);
+    }
   }
 };
+
+  // if (label === "Select Language") {
+  //   return `<select>${label}</select>`
+  // } else if (label === "Your Comment"){
+  //   return `<textarea>${label}</textarea>`
+  // } else
+
+getInfo(formData);
